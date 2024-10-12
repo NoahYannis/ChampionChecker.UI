d8725265c0455f20227136b6b2c73ffd4a7fb600
Noah Yannis Guderjahn <noah.guderjahn@gmx.de>
2024-10-12 21:22:27 +0200

    .

--- tests/ClassControllerTests.php
+++ tests/ClassControllerTests.php
@@ -14,7 +14,7 @@ class ClassControllerTests extends TestCase
     {
         $this->classController = $this->getMockBuilder(ClassController::class) // Class-Controller Mock erstellen
             ->disableOriginalConstructor() // Nicht aufrufen, da config.php in Tests nicht verfügbar ist
-            ->onlyMethods(['getApiData', 'sendApiRequest']) // Zu mockende Methoden
+            ->onlyMethods(['getApiData', 'sendApiRequest', 'getAll']) // Zu mockende Methoden
             ->getMock();
 
         $this->classController->method('sendApiRequest')
@@ -58,13 +58,20 @@ class ClassControllerTests extends TestCase
                     'students' => [],
                     'pointsAchieved' => 10,
                     'classTeacherId' => null
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'EFI22B',
+                    'students' => [],
+                    'pointsAchieved' => 10,
+                    'classTeacherId' => null
                 ]
             ]);
 
-        $classModel = $this->classController->getByName('efi22a'); // Groß- und Kleinschreibung müssen egal sein
+        $classModel = $this->classController->getByName('efi22b'); // Groß- und Kleinschreibung müssen egal sein
         $this->assertInstanceOf(ClassModel::class, $classModel);
-        $this->assertEquals(1, $classModel->getId());
-        $this->assertEquals('EFI22A', $classModel->getName());
+        $this->assertEquals(2, $classModel->getId());
+        $this->assertEquals('EFI22B', $classModel->getName());
     }
 
     public function test_getByName_withNonExistingClass_returnsNull()
@@ -74,5 +81,30 @@ class ClassControllerTests extends TestCase
 
         $classModel = $this->classController->getByName('efi22b');
         $this->assertNull($classModel);
+    }
+
+    public function test_getClassName_WithExistingId_ReturnsClassName()
+    {
+        $this->classController->method('getApiData')
+            ->willReturn([
+
+                'id' => 1,
+                'name' => 'EFI22A',
+                'students' => [],
+                'pointsAchieved' => 10,
+                'classTeacherId' => null,
+            ]);
+
+        $className = $this->classController->getClassName(1);
+        $this->assertEquals('EFI22A', $className);
+    }
+
+    public function test_getClassName_WithNonExistingId_ReturnsQuestionMarks()
+    {
+        $this->classController->method('getApiData')
+            ->willReturn(null);
+
+        $className = $this->classController->getClassName(69);
+        $this->assertEquals('???', $className);
     }
 }
