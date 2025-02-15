906772b18ff0e983e69db92bbe61aaa4f1b9801d
Noah Yannis Guderjahn <noah.guderjahn@gmx.de>
2025-02-15 16:03:02 +0100

    Fehler beim Wechseln von Einzelstationen behoben

--- MVC/View/add_soloresult.php
+++ MVC/View/add_soloresult.php
@@ -130,11 +130,8 @@ include 'nav.php';
 				.then(html => {
 					resultForm.innerHTML = html;
 					submitStationButton.classList.remove("hidden");
-
 					const formScript = resultForm.querySelector("script");
-					const appendedScript = document.createElement("script");
-					appendedScript.textContent = formScript.textContent;
-					document.body.appendChild(appendedScript);
+					eval(formScript.textContent);
 				})
 				.catch(error => console.error("Error loading form:", error));
 		}
