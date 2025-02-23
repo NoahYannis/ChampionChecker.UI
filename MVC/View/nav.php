31d39c657c64f991c9c8e742c779a2ee2744839a
Noah Yannis Guderjahn <noah.guderjahn@gmx.de>
2025-02-23 20:08:07 +0100

    Korrekten Seitentitel anzeigen statt überall Navigation

--- MVC/View/nav.php
+++ MVC/View/nav.php
@@ -29,7 +29,6 @@ $profileImageUrl = $isAuthenticated
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navigation</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet/less" type="text/css" href="../../styles/styles.less" />
     <link rel="stylesheet" type="text/css" href="../../styles/nav.css" />
